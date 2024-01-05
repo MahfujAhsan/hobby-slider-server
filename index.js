ee5bfb1db0509e87e4d91bf3661d666f6346f71c
Mahfuj Ahsan <mahfujur.rahman1@outlook.com>
2024-01-06 03:03:53 +0600

    fetch config.json file

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+
+
 // middlewares
 app.use(cors())
 app.use(express.json());
@@ -34,6 +36,17 @@ async function run() {
             const result = await urlCollection.insertOne(url);
             res.send(result);
         })
+
+        app.get('/api/config', async (req, res) => {
+            try {
+                const response = await fetch('https://bplugins.net/hayat/config.json');
+                const data = await response.json();
+                res.json(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        });
     } finally {
         
     }
